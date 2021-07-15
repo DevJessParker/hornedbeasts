18005eb962ca5bc7ae0900a0fa735b874cc73b8d
DevJessParker <jess.k.dempsey@gmail.com>
2021-07-14 23:04:48 -0700

    lab 02 rework grid img

--- src/hornedbeast.js
+++ src/hornedbeast.js
@@ -1,5 +1,7 @@
 import React from "react";
 import heartpic from "./Red-Heart.png";
+import Card from 'react-bootstrap/Card';
+
 
 class HornedBeast extends React.Component {
 constructor(props) {
@@ -13,18 +15,25 @@ addCounter = () => {
   this.setState({favoriteCount: this.state.favoriteCount + 1})
 }
 
-  render() {
-    return(
-      <>
-        <h2>{this.props.title}</h2>
-        <p>{this.props.description}</p>
-        <img src={this.props.image_url} alt='' onClick={this.addCounter}/>
-        <img src={heartpic} alt=''/>
-        <span>Favorites: {this.state.favoriteCount}</span>
-      </>
+render() {
+  return(
+    <Card class="beastcard">
+    <Card.Img variant="top" src={this.props.image_url} alt='' onClick={this.addCounter} />
+    <Card.Body>
+    <Card.Img variant="top" src={heartpic} onClick={this.addCounter} />
+    <Card.Title>{this.props.title}</Card.Title>
+    <Card.Text>
+    {this.props.description}
+    </Card.Text>
+    <Card.Text>
+    Favorites: {this.state.favoriteCount}
+    </Card.Text>
+    </Card.Body>
+    </Card>
     )
   }
 }
+
 
 
 export default HornedBeast;
